c6a1bf30839f38e7d9285f25cacf8e69bf592bd0
Robert Laws <roblaws@me.com>
2021-01-05 12:46:42 +0300

    update index.js file

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bulma/css/bulma.css';
 import './App.scss';
+import LocationsState from './context/locations/LocationsState';
+import MenusState from './context/menus/MenusState';
+import OrdersState from './context/orders/OrdersState';
+import ModalState from './context/modal/ModalState';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <LocationsState>
+      <MenusState>
+        <OrdersState>
+          <ModalState>
+            <App />
+          </ModalState>
+        </OrdersState>
+      </MenusState>
+    </LocationsState>
   </React.StrictMode>,
   document.getElementById('root')
 );
