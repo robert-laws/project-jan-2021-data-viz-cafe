5223bf146298d5f6a0fb033c77144479971b6396
Robert Laws <roblaws@me.com>
2021-01-09 16:49:27 +0300

    update page not found

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const NotFound = () => {
-  return <div>Page Not Found</div>;
+  return (
+    <section
+      className='section is-flex is-flex-direction-column is-align-items-center is-justify-content-center'
+      style={{ minHeight: '50vh' }}
+    >
+      <div>
+        <h1 className='is-size-1-desktop is-size-2-mobile'>Page Not Found</h1>
+        <h4 className='is-size-4-desktop is-size-5-mobile has-text-centered'>
+          <Link to='/'>Return to the Home Page</Link>
+        </h4>
+      </div>
+    </section>
+  );
 };
