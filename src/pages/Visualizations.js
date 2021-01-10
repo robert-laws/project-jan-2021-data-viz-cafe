1667ddeaa70c0aa85cad1fd22fab93f6181c3e42
Robert Laws <roblaws@me.com>
2021-01-10 15:08:57 +0300

    add column chart to visualizations page

--- src/pages/Visualizations.js
+++ src/pages/Visualizations.js
@@ -36,6 +36,28 @@ export const Visualizations = () => {
           vizElementMenuTable
         );
       }
+
+      if (document.getElementById('viz1610279567451')) {
+        var divElementLocationsChart = document.getElementById(
+          'viz1610279567451'
+        );
+        var vizElementLocationsChart = divElementLocationsChart.getElementsByTagName(
+          'object'
+        )[0];
+
+        if (vizElementLocationsChart) {
+          vizElementLocationsChart.style.width = '100%';
+          vizElementLocationsChart.style.height =
+            divElementLocationsChart.offsetWidth * 0.75 + 'px';
+          var scriptElementLocationsChart = document.createElement('script');
+          scriptElementLocationsChart.src =
+            'https://public.tableau.com/javascripts/api/viz_v1.js';
+          vizElementLocationsChart.parentNode.insertBefore(
+            scriptElementLocationsChart,
+            vizElementLocationsChart
+          );
+        }
+      }
     }
   }, []);
 
@@ -101,7 +123,7 @@ export const Visualizations = () => {
           <noscript>
             <a href='/visualizations'>
               <img
-                alt=' '
+                alt='Data Table of All Menu Items'
                 src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Da&#47;DataVizCafe&#47;MenuItems&#47;1_rss.png'
                 style={{ border: 'none' }}
               />
@@ -120,6 +142,50 @@ export const Visualizations = () => {
             <param
               name='static_image'
               value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Da&#47;DataVizCafe&#47;MenuItems&#47;1.png'
+            />{' '}
+            <param name='animate_transition' value='yes' />
+            <param name='display_static_image' value='yes' />
+            <param name='display_spinner' value='yes' />
+            <param name='display_overlay' value='yes' />
+            <param name='display_count' value='yes' />
+            <param name='language' value='en' />
+            <param name='filter' value='publish=yes' />
+          </object>
+        </div>
+      </section>
+
+      <section className='section'>
+        <Subtitle>Column Chart of Locations by Service Population</Subtitle>
+        <div
+          class='tableauPlaceholder'
+          id='viz1610279567451'
+          style={{ position: 'relative' }}
+        >
+          <noscript>
+            <a href='/visualizations'>
+              <img
+                alt='Column Chart of Locations by Service Population'
+                src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Da&#47;DataVizCafe&#47;LocationsbyServicePopulation&#47;1_rss.png'
+                style={{ border: 'none' }}
+              />
+            </a>
+          </noscript>
+          <object class='tableauViz' style={{ display: 'none' }}>
+            <param
+              name='host_url'
+              value='https%3A%2F%2Fpublic.tableau.com%2F'
+            />{' '}
+            <param name='embed_code_version' value='3' />{' '}
+            <param name='site_root' value='' />
+            <param
+              name='name'
+              value='DataVizCafe&#47;LocationsbyServicePopulation'
+            />
+            <param name='tabs' value='no' />
+            <param name='toolbar' value='yes' />
+            <param
+              name='static_image'
+              value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Da&#47;DataVizCafe&#47;LocationsbyServicePopulation&#47;1.png'
             />{' '}
             <param name='animate_transition' value='yes' />
             <param name='display_static_image' value='yes' />
